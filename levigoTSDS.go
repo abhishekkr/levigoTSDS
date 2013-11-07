9612581ebaa07bcb164b375fea8109e3f0ad62c8
AbhishekKr <abhikumar163@gmail.com>
2013-11-07 10:36:32 +0530

    levigoTSDS base logic to correct time type and ReadNSRecursive

--- levigoTSDS.go
+++ levigoTSDS.go
@@ -9,19 +9,23 @@ import (
   ldbNS "github.com/abhishekkr/levigoNS"
 )
 
-func TimeKeyPart(key_time time) string {
-  return fmt.Sprintf("Time:%q", key_time)
+type Time time.Time
+
+func TimeKeyPart(key_time Time) string {
+  return fmt.Sprintf("%v:%v:%v:%v:%v:%v", key_time.Year(), key_time.Month(),
+                                          key_time.Day(), key_time.Hour(),
+                                          key_time.Minute(), key_time.Second())
 }
 
-func KeyNameSpaceWithTime(key string, key_time time) string{
+func KeyNameSpaceWithTime(key string, key_time Time) string{
   return fmt.Sprintf("%s:%s", key, TimeKeyPart(key_time))
 }
 
-func TimeNameSpaceWithKey(key string, key_time time) string{
+func TimeNameSpaceWithKey(key string, key_time Time) string{
   return fmt.Sprintf("%s:%s", TimeKeyPart(key_time), key)
 }
 
-func KeyAndTimeBothNameSpace(key string, key_time time) string{
+func KeyAndTimeBothNameSpace(key string, key_time Time) (string, string){
   time_ns := TimeKeyPart(key_time)
   return fmt.Sprintf("%s:%s", key, time_ns), fmt.Sprintf("%s:%s", time_ns, key)
 }
@@ -31,7 +35,7 @@ func ReadTSDS(key string, db *levigo.DB){
   fmt.Println(val)
 }
 
-func PushTSDS(key string, val string, key_time time, db *levigo.DB){
+func PushTSDS(key string, val string, key_time Time, db *levigo.DB){
   keytsds := KeyNameSpaceWithTime(key, key_time)
   ldbNS.PushNS(keytsds, val, db)
 }
