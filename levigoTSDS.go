841fb72c8b1cfebeb18939e7a7b5a8735d245e4c
AbhishekKr <abhikumar163@gmail.com>
2014-05-21 21:44:27 +0530

    made all PUSH/DEL tasks return bool as in levigoNS for uniformity

--- levigoTSDS.go
+++ levigoTSDS.go
@@ -1,140 +1,120 @@
 package abklevigoTSDS
 
 import (
-  "fmt"
-  "time"
+	"fmt"
+	"time"
 
-  levigo "github.com/jmhodges/levigo"
+	levigo "github.com/jmhodges/levigo"
 
-  abklevigoNS "github.com/abhishekkr/levigoNS"
-  golhashmap "github.com/abhishekkr/gol/golhashmap"
+	golhashmap "github.com/abhishekkr/gol/golhashmap"
+	abklevigoNS "github.com/abhishekkr/levigoNS"
 )
-
 
 /* Return string of Namespace-d Time-Value for NS-Key */
 func TimeKeyPart(key_time time.Time) string {
-  return fmt.Sprintf("%v:%v:%v:%v:%v:%v", key_time.Year(), key_time.Month(),
-                                          key_time.Day(), key_time.Hour(),
-                                          key_time.Minute(), key_time.Second())
+	return fmt.Sprintf("%v:%v:%v:%v:%v:%v", key_time.Year(), key_time.Month(),
+		key_time.Day(), key_time.Hour(),
+		key_time.Minute(), key_time.Second())
 }
-
 
 /* Return string of Namespace-d Key with Time-Namespace under it */
-func KeyNameSpaceWithTime(key string, key_time time.Time) string{
-  return fmt.Sprintf("%s:%s", key, TimeKeyPart(key_time))
+func KeyNameSpaceWithTime(key string, key_time time.Time) string {
+	return fmt.Sprintf("%s:%s", key, TimeKeyPart(key_time))
 }
-
 
 /* Return string of Namespace-d Time-Namespace with Key under it */
-func TimeNameSpaceWithKey(key string, key_time time.Time) string{
-  return fmt.Sprintf("%s:%s", TimeKeyPart(key_time), key)
+func TimeNameSpaceWithKey(key string, key_time time.Time) string {
+	return fmt.Sprintf("%s:%s", TimeKeyPart(key_time), key)
 }
-
 
 /* Return KeyNameSpaceWithTime and TimeNameSpaceWithKey as multi-return set */
-func KeyAndTimeBothNameSpace(key string, key_time time.Time) (string, string){
-  time_ns := TimeKeyPart(key_time)
-  return fmt.Sprintf("%s:%s", key, time_ns), fmt.Sprintf("%s:%s", time_ns, key)
+func KeyAndTimeBothNameSpace(key string, key_time time.Time) (string, string) {
+	time_ns := TimeKeyPart(key_time)
+	return fmt.Sprintf("%s:%s", key, time_ns), fmt.Sprintf("%s:%s", time_ns, key)
 }
-
 
 /*
 Returns Recursive-Namespaced data under given key, Proxy TSDS
 The desried Time-frame shall be as namespace-d key
 */
-func ReadTSDS(key string, db *levigo.DB) golhashmap.HashMap{
-  return abklevigoNS.ReadNSRecursive(key, db)
+func ReadTSDS(key string, db *levigo.DB) golhashmap.HashMap {
+	return abklevigoNS.ReadNSRecursive(key, db)
 }
-
 
 /*
 Returns Push status for TimeSeries data-store for a key-val for given timestamp
 Default is Key Namespace-d with Time-Namespace under it
 */
-func PushTSDS(key string, val string, key_time time.Time, db *levigo.DB) string{
-  keytsds := KeyNameSpaceWithTime(key, key_time)
-  if abklevigoNS.PushNS(keytsds, val, db) { return keytsds }
-  return ""
+func PushTSDS(key string, val string, key_time time.Time, db *levigo.DB) bool {
+	keytsds := KeyNameSpaceWithTime(key, key_time)
+	return abklevigoNS.PushNS(keytsds, val, db)
 }
-
 
 /* Returns Push status for Key Namespace-d with Time-Namespace under it for given Timestamp */
-func PushTSDS_BaseKey(key string, val string, key_time time.Time, db *levigo.DB) string{
-  return PushTSDS(key, val, key_time, db)
+func PushTSDS_BaseKey(key string, val string, key_time time.Time, db *levigo.DB) bool {
+	return PushTSDS(key, val, key_time, db)
 }
-
 
 /* Returns Push status for Time-Namespace with Key Namespace-d under it for given Timestamp */
-func PushTSDS_BaseTime(key string, val string, key_time time.Time, db *levigo.DB) string{
-  timetsds := TimeNameSpaceWithKey(key, key_time)
-  if abklevigoNS.PushNS(timetsds, val, db) { return timetsds }
-  return ""
+func PushTSDS_BaseTime(key string, val string, key_time time.Time, db *levigo.DB) bool {
+	timetsds := TimeNameSpaceWithKey(key, key_time)
+	return abklevigoNS.PushNS(timetsds, val, db)
 }
-
 
 /*
 Returns Push status for TimeSeries data-store for a key-val for given timestamp
 Both Key and Timestamp base key-vals are creaed
 */
-func PushTSDS_BaseBoth(key string, val string, key_time time.Time, db *levigo.DB) (string, string){
-  keytsds := KeyNameSpaceWithTime(key, key_time)
-  if ! abklevigoNS.PushNS(keytsds, val, db) { keytsds = "" }
+func PushTSDS_BaseBoth(key string, val string, key_time time.Time, db *levigo.DB) (status bool) {
+	keytsds := KeyNameSpaceWithTime(key, key_time)
+	status = abklevigoNS.PushNS(keytsds, val, db)
 
-  timetsds := TimeNameSpaceWithKey(key, key_time)
-  if ! abklevigoNS.PushNS(timetsds, val, db) { timetsds = "" }
+	timetsds := TimeNameSpaceWithKey(key, key_time)
+	status = status && abklevigoNS.PushNS(timetsds, val, db)
 
-  return keytsds, timetsds
+	return status
 }
-
 
 /*
 Returns Push status for TimeSeries data-store for a key-val for time of key-creation
 Default is Key Namespace-d with Time-Namespace under it
 */
-func PushNowTSDS(key string, val string, db *levigo.DB) string{
-  keytsds := KeyNameSpaceWithTime(key, time.Now())
-  if abklevigoNS.PushNS(keytsds, val, db) { return keytsds }
-  return ""
+func PushNowTSDS(key string, val string, db *levigo.DB) bool {
+	keytsds := KeyNameSpaceWithTime(key, time.Now())
+	return abklevigoNS.PushNS(keytsds, val, db)
 }
-
 
 /* Returns Push status for Key Namespace-d with Time-Namespace under it for time of key-creation */
-func PushNowTSDS_BaseKey(key string, val string, db *levigo.DB) string{
-  return PushNowTSDS(key, val, db)
+func PushNowTSDS_BaseKey(key string, val string, db *levigo.DB) bool {
+	return PushNowTSDS(key, val, db)
 }
-
 
 /* Returns Push status for Time-Namespace with Key Namespace-d under it for time of key-creation */
-func PushNowTSDS_BaseTime(key string, val string, db *levigo.DB) string{
-  timetsds := TimeNameSpaceWithKey(key, time.Now())
-  if abklevigoNS.PushNS(timetsds, val, db) { return timetsds }
-  return ""
+func PushNowTSDS_BaseTime(key string, val string, db *levigo.DB) bool {
+	timetsds := TimeNameSpaceWithKey(key, time.Now())
+	return abklevigoNS.PushNS(timetsds, val, db)
 }
-
 
 /*
 Returns Push status for TimeSeries data-store for a key-val for time of key-creation
 Both Key and Timestamp base key-vals are creaed
 */
-func PushNowTSDS_BaseBoth(key string, val string, db *levigo.DB) (string, string){
-  key_time := time.Now()
+func PushNowTSDS_BaseBoth(key string, val string, db *levigo.DB) (status bool) {
+	key_time := time.Now()
 
-  keytsds := KeyNameSpaceWithTime(key, key_time)
-  if ! abklevigoNS.PushNS(keytsds, val, db) { keytsds = "" }
+	keytsds := KeyNameSpaceWithTime(key, key_time)
+	status = abklevigoNS.PushNS(keytsds, val, db)
 
-  timetsds := TimeNameSpaceWithKey(key, key_time)
-  if ! abklevigoNS.PushNS(timetsds, val, db) { timetsds = "" }
+	timetsds := TimeNameSpaceWithKey(key, key_time)
+	status = status && abklevigoNS.PushNS(timetsds, val, db)
 
-  return keytsds, timetsds
+	return status
 }
-
 
 /*
 Returns status to delete Recursive-Namespaced data under given key, Proxy TSDS
 The desried Time-frame shall be as namespace-d key
 */
-func DeleteTSDS(key string, db *levigo.DB) golhashmap.HashMap{
-  abklevigoNS.DeleteNSRecursive(key, db)
-  return abklevigoNS.ReadNSRecursive(key, db)
+func DeleteTSDS(key string, db *levigo.DB) bool {
+	return abklevigoNS.DeleteNSRecursive(key, db)
 }
-
--- levigoTSDS_test.go
+++ levigoTSDS_test.go
@@ -1,236 +1,245 @@
 package abklevigoTSDS
 
 import (
-  "testing"
-  "time"
+	"testing"
+	"time"
 
-  levigo "github.com/jmhodges/levigo"
+	levigo "github.com/jmhodges/levigo"
 
-  abkleveldb "github.com/abhishekkr/levigoNS/leveldb"
-  abklevigoNS "github.com/abhishekkr/levigoNS"
-  golhashmap "github.com/abhishekkr/gol/golhashmap"
+	golhashmap "github.com/abhishekkr/gol/golhashmap"
+	abklevigoNS "github.com/abhishekkr/levigoNS"
+	abkleveldb "github.com/abhishekkr/levigoNS/leveldb"
 )
-
 
 var (
-  dbpath = "/tmp/delete-this-levigoTSDS"
+	dbpath = "/tmp/delete-this-levigoTSDS"
 )
 
-
 func setupTestData(db *levigo.DB) {
-  abklevigoNS.PushNS("upstate:2014:January:2:12:1:20", "down", db)
-  abklevigoNS.PushNS("2014:January:2:12:1:20:upstate", "down", db)
+	abklevigoNS.PushNS("upstate:2014:January:2:12:1:20", "down", db)
+	abklevigoNS.PushNS("2014:January:2:12:1:20:upstate", "down", db)
 
-  abklevigoNS.PushNS("upstate:2014:January:2:12:11:20", "up", db)
-  abklevigoNS.PushNS("2014:January:2:12:11:20:upstate", "up", db)
+	abklevigoNS.PushNS("upstate:2014:January:2:12:11:20", "up", db)
+	abklevigoNS.PushNS("2014:January:2:12:11:20:upstate", "up", db)
 }
-
 
 func TestTimeKeyPart(t *testing.T) {
-  anytime := time.Date(2014, 1, 2, 12, 10, 1, 0, time.UTC)
+	anytime := time.Date(2014, 1, 2, 12, 10, 1, 0, time.UTC)
 
-  expected_val := "2014:January:2:12:10:1"
-  result_val := TimeKeyPart(anytime)
-  if expected_val != result_val {
-    t.Error("Fail: Get", result_val, "instead of", expected_val)
-  }
+	expected_val := "2014:January:2:12:10:1"
+	result_val := TimeKeyPart(anytime)
+	if expected_val != result_val {
+		t.Error("Fail: Get", result_val, "instead of", expected_val)
+	}
 }
-
 
 func TestKeyNameSpaceWithTime(t *testing.T) {
-  anytime := time.Date(2014, 1, 2, 12, 10, 1, 0, time.UTC)
+	anytime := time.Date(2014, 1, 2, 12, 10, 1, 0, time.UTC)
 
-  expected_val := "upstate:2014:January:2:12:10:1"
-  result_val := KeyNameSpaceWithTime("upstate", anytime)
-  if expected_val != result_val {
-    t.Error("Fail: Get", result_val, "instead of", expected_val)
-  }
+	expected_val := "upstate:2014:January:2:12:10:1"
+	result_val := KeyNameSpaceWithTime("upstate", anytime)
+	if expected_val != result_val {
+		t.Error("Fail: Get", result_val, "instead of", expected_val)
+	}
 }
-
 
 func TestTimeNameSpaceWithKey(t *testing.T) {
-  anytime := time.Date(2014, 1, 2, 12, 10, 1, 0, time.UTC)
+	anytime := time.Date(2014, 1, 2, 12, 10, 1, 0, time.UTC)
 
-  expected_val := "2014:January:2:12:10:1:upstate"
-  result_val := TimeNameSpaceWithKey("upstate", anytime)
-  if expected_val != result_val {
-    t.Error("Fail: Get", result_val, "instead of", expected_val)
-  }
+	expected_val := "2014:January:2:12:10:1:upstate"
+	result_val := TimeNameSpaceWithKey("upstate", anytime)
+	if expected_val != result_val {
+		t.Error("Fail: Get", result_val, "instead of", expected_val)
+	}
 }
-
 
 func TestKeyAndTimeBothNameSpace(t *testing.T) {
-  anytime := time.Date(2014, 1, 2, 12, 10, 1, 0, time.UTC)
+	anytime := time.Date(2014, 1, 2, 12, 10, 1, 0, time.UTC)
 
-  expected_val1, expected_val2 := "upstate:2014:January:2:12:10:1", "2014:January:2:12:10:1:upstate"
-  result_val1, result_val2 := KeyAndTimeBothNameSpace("upstate", anytime)
-  if (expected_val1 != result_val1) || (expected_val2 != result_val2) {
-    t.Error("Fail: Get", result_val1, "and", result_val2, "instead of", expected_val1, "and", expected_val2)
-  }
+	expected_val1, expected_val2 := "upstate:2014:January:2:12:10:1", "2014:January:2:12:10:1:upstate"
+	result_val1, result_val2 := KeyAndTimeBothNameSpace("upstate", anytime)
+	if (expected_val1 != result_val1) || (expected_val2 != result_val2) {
+		t.Error("Fail: Get", result_val1, "and", result_val2, "instead of", expected_val1, "and", expected_val2)
+	}
 }
-
 
 func TestReadTSDS(t *testing.T) {
-  db := abkleveldb.CreateDB(dbpath)
-  setupTestData(db)
+	db := abkleveldb.CreateDB(dbpath)
+	setupTestData(db)
 
-  expected_val := "upstate:2014:January:2:12:1:20,down\nupstate:2014:January:2:12:11:20,up\n"
-  result_val := golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("upstate:2014:January", db))
-  if (expected_val != result_val) {
-    t.Error("Fail: Get", result_val, "instead of", expected_val)
-  }
+	expected_val := "upstate:2014:January:2:12:1:20,down\nupstate:2014:January:2:12:11:20,up"
+	result_val := golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("upstate:2014:January", db))
+	if expected_val != result_val {
+		t.Error("Fail: Get", result_val, "instead of", expected_val)
+	}
 
-  expected_val = "upstate:2014:January:2:12:11:20,up\n"
-  result_val = golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("upstate:2014:January:2:12:11", db))
-  if (expected_val != result_val) {
-    t.Error("Fail: Get", result_val, "instead of", expected_val)
-  }
+	expected_val = "upstate:2014:January:2:12:11:20,up"
+	result_val = golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("upstate:2014:January:2:12:11", db))
+	if expected_val != result_val {
+		t.Error("Fail: Get", result_val, "instead of", expected_val)
+	}
 
-  expected_val = ""
-  result_val = golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("upstate:2014:February:2:12:11", db))
-  if (expected_val != result_val) {
-    t.Error("Fail: Get", result_val, "instead of", expected_val)
-  }
+	expected_val = ""
+	result_val = golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("upstate:2014:February:2:12:11", db))
+	if expected_val != result_val {
+		t.Error("Fail: Get", result_val, "instead of", expected_val)
+	}
 
-  abkleveldb.CloseAndDeleteDB(dbpath, db)
+	abkleveldb.CloseAndDeleteDB(dbpath, db)
 }
-
 
 func TestPushTSDS(t *testing.T) {
-  anytime := time.Date(2014, 1, 2, 12, 10, 1, 0, time.UTC)
-  db := abkleveldb.CreateDB(dbpath)
-  setupTestData(db)
+	anytime := time.Date(2014, 1, 2, 12, 10, 1, 0, time.UTC)
+	db := abkleveldb.CreateDB(dbpath)
+	setupTestData(db)
 
-  keytsds := PushTSDS("upstate", "up", anytime, db)
-  expected_val := "upstate:2014:January:2:12:10:1,up\n"
-  result_val := golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("upstate:2014:January:2:12:10:1", db))
-  if (expected_val != result_val) {
-    t.Error("Fail: Get", result_val, "instead of", expected_val)
-  }
-  if keytsds != "upstate:2014:January:2:12:10:1" {
-    t.Error("PushTSDS created wrong key", keytsds, "for upstate:2014:January:2:12:10:1")
-  }
+	if !PushTSDS("upstate", "up", anytime, db) {
+		t.Error("PushTSDS creation failed for upstate:2014:January:2:12:10:1")
+	}
+	expected_val := "upstate:2014:January:2:12:10:1,up"
+	result_val := golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("upstate:2014:January:2:12:10:1", db))
+	if expected_val != result_val {
+		t.Error("Fail: Get", result_val, "instead of", expected_val)
+	}
 
-  abkleveldb.CloseAndDeleteDB(dbpath, db)
+	abkleveldb.CloseAndDeleteDB(dbpath, db)
 }
-
 
 func TestPushTSDS_BaseKey(t *testing.T) {
-  anytime := time.Date(2014, 1, 2, 12, 10, 1, 0, time.UTC)
-  db := abkleveldb.CreateDB(dbpath)
-  setupTestData(db)
+	anytime := time.Date(2014, 1, 2, 12, 10, 1, 0, time.UTC)
+	db := abkleveldb.CreateDB(dbpath)
+	setupTestData(db)
 
-  keytsds := PushTSDS("upstate", "up", anytime, db)
-  expected_val := "upstate:2014:January:2:12:10:1,up\n"
-  result_val := golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("upstate:2014:January:2:12:10:1", db))
-  if (expected_val != result_val) {
-    t.Error("Fail: Get", result_val, "instead of", expected_val)
-  }
-  if keytsds != "upstate:2014:January:2:12:10:1" {
-    t.Error("PushTSDS created wrong key", keytsds, "for upstate:2014:January:2:12:10:1")
-  }
+	if !PushTSDS_BaseKey("upstate", "up", anytime, db) {
+		t.Error("PushTSDS_BaseKey creation failed for upstate:2014:January:2:12:10:1")
+	}
 
-  abkleveldb.CloseAndDeleteDB(dbpath, db)
+	expected_val := "upstate:2014:January:2:12:10:1,up"
+	result_val := golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("upstate:2014:January:2:12:10:1", db))
+	if expected_val != result_val {
+		t.Error("Fail: Get", result_val, "instead of", expected_val)
+	}
+
+	abkleveldb.CloseAndDeleteDB(dbpath, db)
 }
-
 
 func TestPushTSDS_BaseTime(t *testing.T) {
-  anytime := time.Date(2014, 1, 2, 12, 10, 1, 0, time.UTC)
-  db := abkleveldb.CreateDB(dbpath)
-  setupTestData(db)
+	anytime := time.Date(2014, 1, 2, 12, 10, 1, 0, time.UTC)
+	db := abkleveldb.CreateDB(dbpath)
+	setupTestData(db)
 
-  timetsds := PushTSDS_BaseTime("upstate", "up", anytime, db)
-  expected_val := "2014:January:2:12:10:1:upstate,up\n"
-  result_val := golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("2014:January:2:12:10:1:upstate", db))
-  if (expected_val != result_val) {
-    t.Error("Fail: Get", result_val, "instead of", expected_val)
-  }
-  if timetsds != "2014:January:2:12:10:1:upstate" {
-    t.Error("PushTSDS created wrong key", timetsds, "for upstate:2014:January:2:12:10:1")
-  }
+	if !PushTSDS_BaseTime("upstate", "up", anytime, db) {
+		t.Error("PushTSDS_BaseTime creation failed for upstate:2014:January:2:12:10:1")
+	}
+	expected_val := "2014:January:2:12:10:1:upstate,up"
+	result_val := golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("2014:January:2:12:10:1:upstate", db))
+	if expected_val != result_val {
+		t.Error("Fail: Get", result_val, "instead of", expected_val)
+	}
 
-  abkleveldb.CloseAndDeleteDB(dbpath, db)
+	abkleveldb.CloseAndDeleteDB(dbpath, db)
 }
-
-
 
 func TestPushTSDS_BaseBoth(t *testing.T) {
-  anytime := time.Date(2014, 1, 2, 12, 10, 1, 0, time.UTC)
-  db := abkleveldb.CreateDB(dbpath)
-  setupTestData(db)
+	anytime := time.Date(2014, 1, 2, 12, 10, 1, 0, time.UTC)
+	db := abkleveldb.CreateDB(dbpath)
+	setupTestData(db)
 
-  keytsds, timetsds := PushTSDS_BaseBoth("upstate", "up", anytime, db)
-  if (keytsds != "upstate:2014:January:2:12:10:1") && (timetsds != "2014:January:2:12:10:1:upstate") {
-    t.Error("PushTSDS created wrong key", keytsds, "for upstate:2014:January:2:12:10:1")
-  }
+	if !PushTSDS_BaseBoth("upstate", "up", anytime, db) {
+		t.Error("PushTSDS_BaseBoth creation failed for upstate:2014:January:2:12:10:1")
+	}
 
-  expected_val := "2014:January:2:12:10:1:upstate,up\n"
-  result_val := golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("2014:January:2:12:10:1:upstate", db))
-  if (expected_val != result_val) {
-    t.Error("Fail: Get", result_val, "instead of", expected_val)
-  }
+	expected_val := "2014:January:2:12:10:1:upstate,up"
+	result_val := golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("2014:January:2:12:10:1:upstate", db))
+	if expected_val != result_val {
+		t.Error("Fail: Get", result_val, "instead of", expected_val)
+	}
 
-  expected_val = "upstate:2014:January:2:12:10:1,up\n"
-  result_val = golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("upstate:2014:January:2:12:10:1", db))
-  if (expected_val != result_val) {
-    t.Error("Fail: Get", result_val, "instead of", expected_val)
-  }
+	expected_val = "upstate:2014:January:2:12:10:1,up"
+	result_val = golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("upstate:2014:January:2:12:10:1", db))
+	if expected_val != result_val {
+		t.Error("Fail: Get", result_val, "instead of", expected_val)
+	}
 
-  abkleveldb.CloseAndDeleteDB(dbpath, db)
+	abkleveldb.CloseAndDeleteDB(dbpath, db)
 }
-
 
 func TestPushNowTSDS(t *testing.T) {
-  db := abkleveldb.CreateDB(dbpath)
-  setupTestData(db)
+	db := abkleveldb.CreateDB(dbpath)
+	setupTestData(db)
 
-  PushNowTSDS("upstate", "up", db)
+	if !PushNowTSDS("TestPushNowTSDS", "up", db) {
+		t.Error("PushNowTSDS creation failed.")
+	}
 
-  abkleveldb.CloseAndDeleteDB(dbpath, db)
+	result_val := golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("TestPushNowTSDS", db))
+	if len(result_val) == 1 {
+		t.Error("Fail: Wrong count of Key creation")
+	}
+
+	abkleveldb.CloseAndDeleteDB(dbpath, db)
 }
-
 
 func TestPushNowTSDS_BaseKey(t *testing.T) {
-  db := abkleveldb.CreateDB(dbpath)
-  setupTestData(db)
+	db := abkleveldb.CreateDB(dbpath)
+	setupTestData(db)
 
-  PushNowTSDS_BaseKey("upstate", "up", db)
+	if !PushNowTSDS_BaseKey("PushNowTSDS_BaseKey", "up", db) {
+		t.Error("PushNowTSDS_BaseKey creation failed.")
+	}
 
-  abkleveldb.CloseAndDeleteDB(dbpath, db)
+	result_val := golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("PushNowTSDS_BaseKey", db))
+	if len(result_val) == 1 {
+		t.Error("Fail: Wrong count of Key creation")
+	}
+
+	abkleveldb.CloseAndDeleteDB(dbpath, db)
 }
-
 
 func TestPushNowTSDS_BaseTime(t *testing.T) {
-  db := abkleveldb.CreateDB(dbpath)
-  setupTestData(db)
+	db := abkleveldb.CreateDB(dbpath)
+	setupTestData(db)
 
-  PushNowTSDS_BaseTime("upstate", "up", db)
+	if !PushNowTSDS_BaseTime("PushNowTSDS_BaseTime", "up", db) {
+		t.Error("PushNowTSDS_BaseTime creation failed.")
+	}
 
-  abkleveldb.CloseAndDeleteDB(dbpath, db)
+	result_val := golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("PushNowTSDS_BaseTime", db))
+	if len(result_val) == 1 {
+		t.Error("Fail: Wrong count of Key creation")
+	}
+
+	abkleveldb.CloseAndDeleteDB(dbpath, db)
 }
-
 
 func TestPushNowTSDS_BaseBoth(t *testing.T) {
-  db := abkleveldb.CreateDB(dbpath)
-  setupTestData(db)
+	db := abkleveldb.CreateDB(dbpath)
+	setupTestData(db)
 
-  PushNowTSDS_BaseBoth("upstate", "up", db)
+	if !PushNowTSDS_BaseBoth("PushNowTSDS_BaseBoth", "up", db) {
+		t.Error("PushNowTSDS_BaseBoth creation failed.")
+	}
 
-  abkleveldb.CloseAndDeleteDB(dbpath, db)
+	result_val := golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("PushNowTSDS_BaseBoth", db))
+	if len(result_val) == 1 {
+		t.Error("Fail: Wrong count of Key creation")
+	}
+
+	abkleveldb.CloseAndDeleteDB(dbpath, db)
 }
-
 
 func TestDeleteTSDS(t *testing.T) {
-  db := abkleveldb.CreateDB(dbpath)
-  setupTestData(db)
+	db := abkleveldb.CreateDB(dbpath)
+	setupTestData(db)
 
-  DeleteTSDS("upstate:2014:January:2:12", db)
-  expected_val := ""
-  result_val := golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("upstate:2014:January:2:12", db))
-  if (expected_val != result_val) {
-    t.Error("Fail: Get", result_val, "instead of", expected_val)
-  }
+	if !DeleteTSDS("upstate:2014:January:2:12", db) {
+		t.Error("Fail: Deletion of upstate:2014:January:2:12 failed")
+	}
+	expected_val := ""
+	result_val := golhashmap.Hashmap_to_csv(abklevigoNS.ReadNSRecursive("upstate:2014:January:2:12", db))
+	if expected_val != result_val {
+		t.Error("Fail: Get", result_val, "instead of", expected_val)
+	}
 
-  abkleveldb.CloseAndDeleteDB(dbpath, db)
+	abkleveldb.CloseAndDeleteDB(dbpath, db)
 }
-
